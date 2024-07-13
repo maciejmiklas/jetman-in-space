dbf5bd959d7d8050b904ba41fa1302b83bf1e7c5
Miklas, Maciej (StMELF) extern <Maciej.Miklas.extern@stmelf.bayern.de>
2024-07-13 10:16:20 +0200

    use stack to read user input

--- src/in_input.asm
+++ src/in_input.asm
@@ -20,50 +20,50 @@ JoyInput
 	; Key Rright pressed ?
 	LD A, _KB_6_TO_0_HEF						; $EF -> A (6...0)
 	IN A, (_KB_REG_HFE)							; Read keyboard input into A
+	PUSH AF										; Keep A on the stack to avoid rereading the same input
 	BIT 2, A									; Bit 2 reset -> Rright pressed
 	CALL Z, JoyMoveRight
+	POP AF
+
+	; Key Up pressed ?
+	PUSH AF
+	BIT 3, A									; Bit 3 reset -> Up pressed
+	CALL Z, JoyMoveUp
+	POP AF
+
+	; Key Down pressed ?
+	BIT 4, A									; Bit 4 reset -> Down pressed
+	CALL Z, JoyMoveDown	
 
 	; Joystick right pressed ?
 	LD A, _JOY_MASK_H20							; Activete joystick register
 	IN A, (_JOY_REG_H1F) 						; Read joystick input into A
+	PUSH AF										; Keep A on the stack to avoid rereading the same input
 	BIT 0, A									; Bit 0 set -> Right pressed
-	CALL NZ, JoyMoveRight			
-
-	; Key Left pressed ?
-	LD A, _KB_5_TO_1_HF7						; $FD -> A (5...1)
-	IN A, (_KB_REG_HFE)							; Read keyboard input into A
-	BIT 4, A									; Bit 4 reset -> Left pressed
-	CALL Z, JoyMoveLeft		
+	CALL NZ, JoyMoveRight	
+	POP AF
 
 	; Joystick left pressed ?
-	LD A, _JOY_MASK_H20							; Activete joystick register
-	IN A, (_JOY_REG_H1F) 						; Read joystick input into A
+	PUSH AF
 	BIT 1, A									; Bit 1 set -> Left pressed
 	CALL NZ, JoyMoveLeft
-
-	; Key Up pressed ?
-	LD A, _KB_6_TO_0_HEF						; $EF -> A (6...0)
-	IN A, (_KB_REG_HFE)							; Read keyboard input into A
-	BIT 3, A									; Bit 3 reset -> Up pressed
-	CALL Z, JoyMoveUp	
-
-	; Joystick up pressed ?
-	LD A, _JOY_MASK_H20							; Activete joystick register
-	IN A, (_JOY_REG_H1F) 						; Read joystick input into A
-	BIT 3, A									; Bit 3 set -> Up pressed
-	CALL NZ, JoyMoveUp
-
-	; Key Down pressed ?
-	LD A, _KB_6_TO_0_HEF						; $EF -> A (6...0)
-	IN A, (_KB_REG_HFE)							; Read keyboard input into A
-	BIT 4, A									; Bit 4 reset -> Down pressed
-	CALL Z, JoyMoveDown				
+	POP AF
 
 	; Joystick down pressed ?
-	LD A, _JOY_MASK_H20							; Activete joystick register
-	IN A, (_JOY_REG_H1F) 						; Read joystick input into A
+	PUSH AF
 	BIT 2, A									; Bit 2 set -> Down pressed
 	CALL NZ, JoyMoveDown
+	POP AF
+
+	; Joystick fire pressed ?
+	PUSH AF
+	AND %01110000								; Any of three fires pressed?
+	CALL NZ, JoyPressFire	
+	POP AF
+
+	; Joystick up pressed ?
+	BIT 3, A									; Bit 3 set -> Up pressed
+	CALL NZ, JoyMoveUp
 
 	; Key Fire (Z) pressed ?
 	LD A, _KB_V_TO_Z_HFE						; $FD -> A (5...1)
@@ -71,12 +71,12 @@ JoyInput
 	BIT 1, A									; Bit 1 reset -> Z pressed
 	CALL Z, JoyPressFire
 
-	; Joystick fire pressed ?
-	LD A, _JOY_MASK_H20							; Activete joystick register
-	IN A, (_JOY_REG_H1F) 						; Read joystick input into A
-	AND %01110000								; Any of three fires pressed?
-	CALL NZ, JoyPressFire	
-	
+	; Key Left pressed ?
+	LD A, _KB_5_TO_1_HF7						; $FD -> A (5...1)
+	IN A, (_KB_REG_HFE)							; Read keyboard input into A
+	BIT 4, A									; Bit 4 reset -> Left pressed
+	CALL Z, JoyMoveLeft		
+
 	CALL JoyEnd
 
 	RET											; END JoyInput
