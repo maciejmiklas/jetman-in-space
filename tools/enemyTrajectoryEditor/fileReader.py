e45dfe95c050c6d725d9dadb554832c731183395
dawidmazurek4 <dawid5mazurek@gmail.com>
2025-10-15 10:54:01 +0300

    changing directory

---
+++ tools/enemyTrajectoryEditor/fileReader.py
@@ -0,0 +1,17 @@
+def read_tilemap(filename, width, height):
+    tilemap = []
+    tile_count = (width * height) * 2
+    start_offset = 0x00
+
+    with open(filename, "rb") as file:
+        file.seek(start_offset)
+        data = file.read(tile_count)
+
+    for i, byte in enumerate(data):
+        if i % 2 != 0:
+            continue
+        if byte == 57:
+            tilemap.append(0)
+        else:
+            tilemap.append(1)
+    return tilemap
