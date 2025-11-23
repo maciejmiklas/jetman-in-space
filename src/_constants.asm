1f7184122c7e5adbdd8f6df26a4be16af2ecb3b2
Miklas, Maciej <maciej@miklas.biz>
2025-11-23 10:40:56 +0100

    dev

--- src/_constants.asm
+++ src/_constants.asm
@@ -3,7 +3,7 @@
 ;----------------------------------------------------------;
 ; Lots of documentation copied from https://wiki.specnext.dev
 
-gameVersion             DB "V0.67 UNDER CONSTRUCTION"
+gameVersion             DB "V0.68 UNDER CONSTRUCTION"
 GAME_VERSION_SIZE       = 25
 
 ;----------------------------------------------------------;
