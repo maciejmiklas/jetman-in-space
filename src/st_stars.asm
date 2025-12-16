0924cba17e8198207e2157a485519a53dc77b541
Miklas, Maciej <maciej@miklas.biz>
2025-12-16 18:21:34 +0100

    dev

--- src/st_stars.asm
+++ src/st_stars.asm
@@ -37,14 +37,14 @@ ST_MOVE_DOWN            = 4
 
 starsState              DB ST_SHOW
 
-ST_L1_MOVE_DEL_D10      = 8                      ; Stars move delay for layer 1.
-ST_L2_MOVE_DEL_D2       = 2                      ; Stars move delay for layer 2.
+ST_L1_MOVE_DEL_D10      = 8                     ; Stars move delay for layer 1.
+ST_L2_MOVE_DEL_D2       = 2                     ; Stars move delay for layer 2.
 
-starsMoveL1Delay        DB ST_L1_MOVE_DEL_D10    ; Delay counter for stars on layer 1 (there are 2 layers of stars).
+starsMoveL1Delay        DB ST_L1_MOVE_DEL_D10   ; Delay counter for stars on layer 1 (there are 2 layers of stars).
 starsMoveL2Delay        DB ST_L2_MOVE_DEL_D2    ; Delay counter for stars on layer 2.
 
 randColor               DB 0                    ; Rand value from the previous call.
-; A0 = 0000 1010
+
 ; Currently rendered palette
 starsPalPoint           DW 0
 starsPalSize            DB 0
