52f66c2e7bebffda2d3de4146ec3e9c83b76c8c9
Miklas, Maciej <maciej@miklas.biz>
2026-02-23 08:12:04 +0100

    dev

--- src/main.asm
+++ src/main.asm
@@ -32,11 +32,11 @@ start
 
     CALL gc.SetupSystem
 
-    LD A, 7
-    LD (ll.currentLevel), A
-    CALL gc.LoadCurrentLevel
+    ;LD A, 3
+    ;LD (ll.currentLevel), A
+    ;CALL gc.LoadCurrentLevel
     
-    ;CALL gc.LoadMainMenu
+    CALL gc.LoadMainMenu
 
     ; ##########################################
     ; Music
