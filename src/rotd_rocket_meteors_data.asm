a0bad99ebb1e0609138962054f6221620a8be849
Miklas, Maciej <maciej@miklas.biz>
2026-04-22 19:11:14 +0200

    harder difficulty for meteor showers

--- src/rotd_rocket_meteors_data.asm
+++ src/rotd_rocket_meteors_data.asm
@@ -20,53 +20,50 @@ asDeploy1
     rot.MED {300, 150, 1,       rot.MP2,     rot.AS_ACTIVE_NO} ; 6
 
 ; See rotd.randMovAddr
-randMov1                DB 0,1,  0,1|$80, 3,1,  3,1|$80,  5,1,  5,1|$80,  2,1,  2,1|$80,  6,1,  1,1, 5,1,  5,1|$80,  0,1
-                        DB 2,1,  4,1,  6,2,  6,1|$80,  0,1,  1,1,  4,1,  6,1,  5,1,  0,1|$80,  3,3,  3,1|$80,  2,1,  5,2,  6,1|$80
-                        DB 3,1|$80,  4,1|$80
+randMov1
+    DB 00,1|$00, 01,1|$80, 03,1|$00, 03,1|$80, 05,1|$00, 05,1|$80, 02,1|$00, 02,1|$80, 06,1|$00, 01,1|$00, 05,1|$00, 01,1|$80, 00,1|$00
+    DB 02,1|$00, 04,1|$00, 06,2|$00, 06,1|$80, 00,1|$00, 01,1|$00, 04,1|$80, 06,1|$00, 05,1|$00, 00,1|$80, 03,3|$00, 03,1|$80, 02,1|$00
+    DB 05,1|$80, 06,1|$80, 01,1|$80, 04,1|$80
+    ASSERT $ - randMov1 == 2 * rot.RAND_MOVE_SIZE_D30
 
 asDeploy2
 ;            X    Y    MOVE_SPD MOVE_PAT     ACTIVE
-    rot.MED {300, 050, 1,       rot.MP2,     rot.AS_ACTIVE_NO} ; 0
-    rot.MED {010, 000, 1,       rot.MP1,     rot.AS_ACTIVE_NO} ; 1
+    rot.MED {300, 050, 3,       rot.MP2,     rot.AS_ACTIVE_NO} ; 0
+    rot.MED {010, 000, 2,       rot.MP1,     rot.AS_ACTIVE_NO} ; 1
     rot.MED {050, 000, 2,       rot.MP1,     rot.AS_ACTIVE_NO} ; 2
     rot.MED {300, 100, 3,       rot.MP2,     rot.AS_ACTIVE_NO} ; 3
     rot.MED {130, 000, 2,       rot.MP1,     rot.AS_ACTIVE_NO} ; 4
-    rot.MED {180, 000, 1,       rot.MP1,     rot.AS_ACTIVE_NO} ; 5
-    rot.MED {300, 200, 2,       rot.MP2,     rot.AS_ACTIVE_NO} ; 6
+    rot.MED {180, 000, 2,       rot.MP1,     rot.AS_ACTIVE_NO} ; 5
+    rot.MED {300, 200, 3,       rot.MP2,     rot.AS_ACTIVE_NO} ; 6
 
 ; See rotd.randMovAddr
-randMov2                DB 0,1,  3,1|$80, 5,2,  3,1|$80,  2,1,  5,1,  3,1|$80,  5,1|$80,  2,1|$80,  3,1, 5,3|$80,  1,2|$80,  3,3|$80
-                        DB 0,1|$80,  2,1|$80,  4,1|$80,  6,1,  1,2,  3,2,  5,2,  6,3,  1,2,  3,1,  6,1|$80,  0,1,  2,2,  5,1|$80,  4,2
-                        DB 1,1,  0,2
+randMov2
+    DB 02,1|$00, 03,1|$80, 01,1|$00, 03,1|$80, 01,1|$00, 06,1|$80, 0,1|$00, 02,1|$80, 06,1|$00, 01,1|$00, 05,1|$00, 04,1|$00, 00,1|$00
+    DB 02,1|$00, 04,1|$00, 05,1|$00, 06,1|$80, 00,1|$80, 02,1|$80, 04,1|$80, 00,1|$80, 05,1|$00, 00,1|$80, 03,1|$80, 03,1|$80, 02,1|$00
+    DB 05,1|$80, 06,1|$80, 01,1|$80, 04,1|$80
+    ASSERT $ - randMov2 == 2 * rot.RAND_MOVE_SIZE_D30
 
 asDeploy3
 ;            X    Y    MOVE_SPD MOVE_PAT     ACTIVE
-    rot.MED {300, 020, 1,       rot.MP2,     rot.AS_ACTIVE_NO} ; 0
-    rot.MED {010, 000, 2,       rot.MP1,     rot.AS_ACTIVE_NO} ; 1
+    rot.MED {300, 020, 2,       rot.MP2,     rot.AS_ACTIVE_NO} ; 0
+    rot.MED {010, 000, 3,       rot.MP1,     rot.AS_ACTIVE_NO} ; 1
     rot.MED {300, 050, 1,       rot.MP2,     rot.AS_ACTIVE_NO} ; 2
     rot.MED {300, 120, 2,       rot.MP2,     rot.AS_ACTIVE_NO} ; 3
-    rot.MED {150, 000, 1,       rot.MP1,     rot.AS_ACTIVE_NO} ; 4
-    rot.MED {200, 000, 2,       rot.MP1,     rot.AS_ACTIVE_NO} ; 5
+    rot.MED {150, 000, 2,       rot.MP1,     rot.AS_ACTIVE_NO} ; 4
+    rot.MED {200, 000, 3,       rot.MP1,     rot.AS_ACTIVE_NO} ; 5
     rot.MED {300, 200, 1,       rot.MP2,     rot.AS_ACTIVE_NO} ; 6
 
-randMov3                 DB 0,2,  3,2,  5,2,  0,1|$80,  3,2|$80,  5,2|$80,  1,1,  2,1,  6,1,  1,1|$80,  2,1|$80,  4,1|$80 
-                        DB 0,5|$80,  1,5|$80,  2,5|$80,  3,5|$80,  4,5|$80,  5,5|$80,  6,5|$80
-                        DB 0,2, 2,2, 1,1, 3,2, 4,1, 5,2, 6,1, 0,1, 3,1, 5,1
 
 ; See rotd.randMovAddr
 asDeploy4
 ;            X    Y    MOVE_SPD MOVE_PAT     ACTIVE
     rot.MED {300, 020, 2,       rot.MP2,     rot.AS_ACTIVE_NO} ; 0
     rot.MED {010, 000, 3,       rot.MP1,     rot.AS_ACTIVE_NO} ; 1
-    rot.MED {300, 050, 1,       rot.MP2,     rot.AS_ACTIVE_NO} ; 2
+    rot.MED {300, 050, 5,       rot.MP2,     rot.AS_ACTIVE_NO} ; 2
     rot.MED {300, 120, 3,       rot.MP2,     rot.AS_ACTIVE_NO} ; 3
     rot.MED {150, 000, 2,       rot.MP1,     rot.AS_ACTIVE_NO} ; 4
-    rot.MED {200, 000, 1,       rot.MP1,     rot.AS_ACTIVE_NO} ; 5
+    rot.MED {200, 000, 5,       rot.MP1,     rot.AS_ACTIVE_NO} ; 5
     rot.MED {300, 200, 4,       rot.MP2,     rot.AS_ACTIVE_NO} ; 6
-
-randMov4                DB 0,2,  3,2,  5,2,  0,2|$80,  3,2|$80,  5,2|$80,  1,1,  2,1,  4,1,  6,1|$80,  2,1|$80,  4,1|$80 
-                        DB 1,1|$80,  0,1,  2,1|$80,  5,1, 5,1,  5,1|$80,  6,1|$80
-                        DB 0,2, 1,2, 2,3, 3,2, 4,1, 5,3, 6,2, 0,1, 3,2, 5,3
 
 ;----------------------------------------------------------;
 ;                       ENDMODULE                          ;
